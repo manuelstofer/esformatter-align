f61cd6416e3e3efc62ddf395009c4b29382cdae5
Manuel Stofer <manuelstofer@gmail.com>
2015-11-24 17:44:00 +0100

    Bugfix: Align nodes only once

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var rocambole = require('rocambole');
 var flatten   = require('array-flatten');
 
-exports.setOptions = function(opts) {};
+var alignedNodes = [];
+
+exports.setOptions = function(opts) {
+  alignedNodes = [];
+};
 
 exports.transform = function(ast) {
   rocambole.walk(ast, function(node) {
@@ -24,11 +28,16 @@ function alignObjectExpression(node) {
 }
 
 function alignVariableDeclaration(node) {
+  if (alignedNodes.indexOf(node) !== -1) return;
+
   var nodes = [];
   while (node && node.type == 'VariableDeclaration') {
     nodes.push(node);
     node = node.next;
   }
+
+  alignedNodes = alignedNodes.concat(nodes);
+
   var tokens = flatten(nodes.map(function(node) {
     return node.declarations
       .map(function(declaraction) {
@@ -64,23 +73,27 @@ function groupConsecutive(tokens) {
 }
 
 function alignTokens(tokens) {
-  var alignToCol = Math.max.apply(Math, tokens.map(getTokenColumn));
+  var alignToCol = Math.max.apply(Math, tokens.map(getMinTokenColumn));
   tokens.forEach(function(token) {
     var whitespace = token.prev;
     if (!isWhiteSpace(whitespace)) {
       throw new Error('no whitespace before token to align');
     }
-    var whitespaceDiff   = alignToCol - getTokenColumn(token);
-    var whitespaceLength = whitespace.value.length + whitespaceDiff;
-    whitespace.value = repeat(' ', whitespaceLength);
+    var alignDiff = alignToCol - getMinTokenColumn(token);
+    whitespace.value = repeat(' ', alignDiff + 1);
   });
 }
 
-function getTokenColumn(token) {
+function getMinTokenColumn(token) {
   var lineFirst = findPrevious(token.prev, isFirstOfLine);
   var pos       = 0;
   for (var t = lineFirst; t !== token; t = t.next) {
-    pos += t.value.length;
+    if (isWhiteSpace(t) && t == token.prev) {
+      pos += 1;
+    } else {
+      pos += t.value.length;
+    }
+
   }
   return pos;
 }
